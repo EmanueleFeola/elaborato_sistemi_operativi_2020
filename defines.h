11ce8301905aba3c464f613449fae7521e4a365e
Ema1st <39267789+Ema1st@users.noreply.github.com>
2020-05-21 00:19:47 +0200

    Update defines.h

--- defines.h
+++ defines.h
@@ -3,3 +3,27 @@
 ///         e funzioni specifiche del progetto.
 
 #pragma once
+
+// number of devices
+#define NDEVICES 5
+
+// board settings
+#define ROWS 5
+#define COLS 5
+
+typedef struct nextMove{
+    int row;
+    int col;
+} nextMove_t;
+
+typedef struct {
+    pid_t pid_sender;
+    pid_t pid_receiver;
+    int message_id;
+    char message[256];
+    int max_distance;
+} Message;
+
+void fillNextLine(int fd, char input[]);
+void fillNextMove(char *nextLine, int nchild, nextMove_t *nextMove);
+void checkEuclideanDistance(int nchild, char *fifoPath, nextMove_t nextMove);
