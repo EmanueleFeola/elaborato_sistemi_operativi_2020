d3dcc11a40e5a83f8ee0621bee4293ecec55575f
Ema1st <39267789+Ema1st@users.noreply.github.com>
2020-05-22 00:16:58 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,63 +1,87 @@
 /// @file client.c
 /// @brief Contiene l'implementazione del client.
 
+#include <signal.h> 
+#include <stdio.h> 
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <string.h>
+#include <sys/shm.h>
 
+#include "semaphore.h"
+#include "defines.h"
 #include "err_exit.h"
 
-#include "defines.h"
-//#include "message_fifo.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SCRIPT_INPUT
 
 int main(int argc, char * argv[]) {
+    // ./client pid_receiver message_id message max_distance
 
-   if (argc != 1) {
-        printf("Il nome dell'eseguibile e' sbagliato\n");//DUE PROCESSI, IN QUESTO CASO SCOLLEGATI L'UNO DALL'ALTRO, NON POSSONO CONDIVIDERE IL NOME DI UN file SE NON GLIELO PASSIAMO A MENO CHE NON UTILIZZIAMO LA MEMORIA CONDIVISA.
-        return 0;
-    }
+    Message msg;
+    msg.pid_sender = getpid();
 
-  //LEGGE IL PATHNAME DELLA FIFO
-  char *path2ServerFIFO = "/tmp/dev_fifo.DevicenumDev";
-
-  printf("<Client - Device> opening FIFO %s...\n", path2ServerFIFO);
-
-  int serverFIFO = open(path2ServerFIFO, O_WRONLY);
-  if (serverFIFO == -1)
-      errExit("open failed");
+    // fifo path
+    char *fifoBasePath = "/tmp/dev_fifo.";
+    char fname[50] = {0};
+    strcat(fname, fifoBasePath);
 
 
-  /*int rows = 0;  
-  char c;
-  do {  
-      printf("\nCaro utente, inserisci un numero da 1 a 5 (il numero del device a cui vuoi inviare il tuo messaggio):\n ");
-  } while (((scanf("%d%c", &rows, &c)!=2 || c!='\n') && clean_stdin()) || rows<1 || rows>5);//Usare scanf("%d",&rows) invece di scanf("%s",input) ci consente di ottenere direttamente il valore intero da stdin senza la necessità di convertirlo in int. Se l'utente immette una stringa contenente caratteri non numerici è necessario pulire lo stdin prima del successivo scanf("%d",&rows).
+    #ifdef SCRIPT_INPUT
+        if(argc != 5){
+            // perchè non va la errExit dc
+            printf("<client> ./client takes 4 parameters: ./client pid_receiver message_id message max_distance\n");
+            exit(1);
+        }
 
- // struct messagefifo MessageFifo;*/
+        strcat(fname, argv[1]);
+        msg.pid_receiver = atoi(argv[1]);
+        msg.message_id = atoi(argv[2]);
 
-  char messaggio[256];  
+        memcpy(msg.message, argv[3], strlen(argv[3]));//dato che e' una stringa devo fare una memcpy e il msg e' un puntatore ad un area di memoria, devo copiare da dove inizia msg alla sua fine e lo copio tramite memcpy
+        msg.message[strlen(argv[3])] = '\0';//bisogna sempre aggiungere un carattere di fine stringa per sicurezza
+        
+        msg.max_distance = atoi(argv[4]);
 
-  printf("Caro utente, inserisci il tuo messaggio: \n ");
-  scanf("%s ", messaggio);
+    #else
+        char pidString[7] = {0};
+        printf("<client %d> Insert destination PID:\n", getpid());
+        scanf("%s", pidString);
+        strcat(fname, pidString);
+        msg.pid_receiver = atoi(pidString);
 
-  int len1 = strlen(messaggio);
+        printf("<client %d> Insert message id: \n", getpid());
+        scanf("%d", msg.message_id);
 
-  printf("<Client - Device> sending '%s'\n", messaggio);
+        printf("<client %d> Insert message: \n", getpid());
+        scanf("%s", msg.message);
 
-  if (write(serverFIFO, messaggio, len1) != len1)
-        errExit("write failed");
+        printf("<client %d> Insert max distance: \n", getpid());
+        scanf("%d", msg.max_distance);
+    #endif
 
-  // Close the FIFO
-  if (close(serverFIFO) != 0)
-      errExit("close failed");
+    printf("<client> Created message:\n\
+    pid_sender: %d\n\
+    pid_receiver: %d\n\
+    message_id: %d\n\
+    message: |%s|\n\
+    max_distance: %d\n",
+     msg.pid_sender, msg.pid_receiver, msg.message_id, msg.message, msg.max_distance);
+    
+    printf("<client> Sending to fifo: %s\n", fname);
+
+    int fd = open(fname, O_WRONLY);
+    if(fd == -1)
+        printf("<client %d> failed to open fifo\n", getpid());
+
+    write(fd, &msg, sizeof(msg));
+    close(fd);
+
+    return 0;
 }
