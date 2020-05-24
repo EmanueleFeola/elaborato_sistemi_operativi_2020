3e9de90b82b01e77ef68413758c03578a8c3eac8
Ema1st <39267789+Ema1st@users.noreply.github.com>
2020-05-25 00:16:03 +0200

    Update defines.h

--- defines.h
+++ defines.h
@@ -1,7 +1,7 @@
 /// @file defines.h
 /// @brief Contiene la definizioni di variabili
 ///         e funzioni specifiche del progetto.
-
+#include <sys/types.h>
 #pragma once
 
 // number of devices
@@ -26,4 +26,4 @@ typedef struct {
 
 void fillNextLine(int fd, char input[]);
 void fillNextMove(char *nextLine, int nchild, nextMove_t *nextMove);
-void checkEuclideanDistance(int nchild, char *fifoPath, nextMove_t *nextMove, nextMove_t *nextMove_nchild);
+void checkEuclideanDistance(char *fifoPath, nextMove_t *nextMove, nextMove_t *nextMove_nchild, int max_distance);
